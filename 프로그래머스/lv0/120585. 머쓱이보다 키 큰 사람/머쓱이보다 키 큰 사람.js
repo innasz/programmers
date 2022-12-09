d7eacab9de16c4cef89870b5534cd8c70f86f400
innasz <106691326+innasz@users.noreply.github.com>
2022-12-09 10:21:17 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,9 +1,8 @@
-function solution (array, height) {
+function solution(array, height) {
     let count = 0;
     for(let i = 0; i < array.length; i++){
         if(array[i] > height){
             count++
-        }
-    }
-    return count;
+        } 
+    }return count;
 }
