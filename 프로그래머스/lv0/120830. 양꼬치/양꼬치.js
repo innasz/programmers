d24a5b84dc480325b54a3dba4f371c7887d51764
innasz <106691326+innasz@users.noreply.github.com>
2022-12-09 09:42:50 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,5 +1,3 @@
 function solution(n, k) {
-    let answer = 
-        (12000 * n) + (2000 * k) - (2000 * Math.floor(n * 1/10))
-    return answer;
-}
+   return 12000 * n + 2000 * k - parseInt(n/10) * 2000 
+} 
